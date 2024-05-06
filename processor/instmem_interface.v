0c956a33b747643e5b9d82e8f90c623798765dac
Allen Tan <allen.jason.tan@eee.upd.edu.ph>
2024-05-07 03:00:16 +0800

    More work on instmem interface

--- processor/instmem_interface.v
+++ processor/instmem_interface.v
@@ -49,51 +49,14 @@ module instmem_interface (
     
     wire busy;
     reg issue_req;
-    
-    
-    /*
-        Types of instruction ops:
         
-        (Unfilled buffer)
-        - Issue Request
-        - Wait for Ready
-        - Read, check compressed, issue instruction, fill buffer
-        
-        (Filled buffer)
-        - Check buffer compressed [, issue instruction OR issue request]
-        [- Wait for Ready]
-        [- Read, issue instruction, fill buffer]
-        
-        (Jump/Branch)
-        - Check alignment, Issue Request
-        - Wait for Ready
-            (Aligned)
-            - Read, check compressed, issue instruction, fill buffer
-            (Misaligned)
-            - Check buffer compressed [, issue instruction OR issue request]
-            [- Wait for Ready]
-            [- Read, issue instruction, fill buffer]
-    */
-    
-    /*
-        Decoupled processes:
-        
-        REQUEST
-        
-        [MEMORY DRIVER]
-        
-        BUFFER, ISSUE INSTRUCTIONS
-    */
-    
-    // REQUEST [INPUT]
-    
     reg [`PC_ADDR_BITS-1:0] inst_addr_t;
     wire issue_req = !id_stall;
     // push effective address into driver
 
     always@(posedge clk) begin
         if (!nrst) begin
-            pc <= '0;
+            pc <= 32'd0;
         end
         else begin
             if (busy_i) begin
@@ -131,216 +94,165 @@ module instmem_interface (
         .gnt(im_gnt),
         .valid(im_valid)
     );
+
+endmodule
+
+module _pc_x(
+    input clk,
+    input nrst,
+    input id_stall,
+    input if_flush,
     
-    // DISPATCH [OUTPUT]
+    input [`PC_ADDR_BITS-1:0] if_pc4,
+    input [`PC_ADDR_BITS-1:0] if_pcnew,
+    input branch,
+    input read,
     
-    reg [2:0] n_state;
+    output reg [`PC_ADDR_BITS-1:0] pc_out,
+    output reg branch_o
+    );
     
-    localparam I_RESET = 3'd0;
-    localparam I_NEXT_N = 3'd1;
-    localparam I_NEXT_F = 3'd2;
-    localparam I_BRANCH_A = 3'd3;
-    localparam I_BRANCH_M1 = 3'd4;
-    localparam I_BRANCH_M2 = 3'd5;
+    reg [1:0] fill;
+    reg [`PC_ADDR_BITS-1:0] addr_buffer [3:0];
+    reg branch_buffer [3:0];
+    reg [`PC_ADDR_BITS-1:0] pc_next;
     
-    always@ (posedge clk) begin
+    always@ (*) begin
+        case(fill)
+            2'd0: begin
+                if (id_stall || if_flush) begin
+                    pc_next = pc_out;
+                    branch_o = 0;
+                end
+                else begin
+                    pc_next = if_pcnew;
+                    branch_o = branch;
+                end
+            end
+            default: begin
+                pc_next = addr_buffer[fill-1];
+                branch_o = branch_buffer [fill-1];
+            end
+        endcase
+    end
+    
+    always@(posedge clk) begin
         if (!nrst) begin
-            n_state <= 3'd0;
+            pc_out <= 32'd0;
         end
         else begin
-            case(n_state)
-                I_RESET:
-                    n_state <= 3'd0;
-                default:
-                    n_state <= 3'd0;
-            endcase
+            pc_out <= pc_next;
         end
     end
     
-	/*
-	always@(posedge clk) begin
-	   if (!nrst) begin
-	       comp_buffer <= 16'd0;
-	       jump_buffer <= 16'd0;
-           pc <= 32'd0;
-           state <= 3'd0;
-           filled <= 1'b0;
-           in_branch <= 0;
-	   end
-	   else begin
-            if (id_stall) begin
-                comp_buffer <= comp_buffer;
-                jump_buffer <= jump_buffer;
-                pc <= pc;
-                filled <= filled;   
-                in_branch <= in_branch;
-            end
-            else begin
-                case(state)
-                    // Default state
-                    3'd0: begin
-                        case ({branch, in_branch})
-                            // regular instruction
-                            2'b00: begin
-                                in_branch <= 0;
-                                comp_buffer <= inst_data[31:16];
-                                jump_buffer <= 16'd0;
-                                pc <= if_pcnew;
-                                state <= 3'd0;
-                                filled <= (align ^ inst_compressed);
-                            end
-                            // start control flow instruction
-                            2'b10: begin
-                                state <= 3'd0;
-                                if (align_branch) begin
-                                    // Branch into misaligned instruction!
-                                    // check lowest two bits of first halfword
-                                    if (misalign_in_compressed) begin
-                                        // 16-bit instruction
-                                        // affected by jump_flush --> resolve on next cycle
-                                        pc <= if_pcnew;
-                                        in_branch <= 0;
-                                        comp_buffer <= inst_data[31:16];
-                                        jump_buffer <= 16'd0;
-                                        filled <= 1'b1; 
-                                    end
-                                    else begin
-                                        pc <= if_pcnew;
-                                        in_branch <= 1;
-                                        comp_buffer <= inst_data[31:16];
-                                        jump_buffer <= inst_data[15:0];  
-                                        filled <= 1'b1; 
-                                    end
-                                end
-                                else begin
-                                    // check lowest two bits instead
-                                    if (inst_data[1:0] == 2'd3) begin
-                                        // word-aligned 32-bit instruction
-                                        pc <= if_pcnew;
-                                        in_branch <= 0;
-                                        comp_buffer <= 16'd0;
-                                        jump_buffer <= 16'd0;
-                                        filled <= 1'b0;
-                                    end
-                                    else begin
-                                        // word-aligned 16-bit instruction 
-                                        pc <= if_pcnew;
-                                        in_branch <= 0;
-                                        comp_buffer <= inst_data[31:16];
-                                        jump_buffer <= 16'd0;
-                                        filled <= 1'b1; 
-                                    end
-                                end
-                            end
-                            // in control flow instruction
-                            2'b01: begin
-                                comp_buffer <= inst_data[31:16];
-                                jump_buffer <= 16'd0;
-                                state <= 3'd0;
-                                pc <= pc + 32'd4;
-                                filled <= 1'b1;
-                                in_branch <= 0;
-                            end
-                            // control flow instruction into another control flow instruction
-                            2'b11: begin
-                                state <= 3'd0;
-                                if (align_branch) begin
-                                    // Branch into misaligned instruction!
-                                    // check lowest two bits of first halfword
-                                    if (misalign_in_compressed) begin
-                                        // 16-bit instruction
-                                        // affected by jump_flush --> resolve on next cycle
-                                        pc <= if_pcnew;
-                                        in_branch <= 0;
-                                        comp_buffer <= inst_data[31:16];
-                                        jump_buffer <= 16'd0;
-                                        filled <= 1'b1; 
-                                    end
-                                    else begin
-                                        pc <= if_pcnew;
-                                        in_branch <= 1;
-                                        comp_buffer <= inst_data[31:16];
-                                        jump_buffer <= inst_data[15:0];  
-                                        filled <= 1'b1; 
-                                    end
-                                end
-                                else begin
-                                    // check lowest two bits instead
-                                    if (inst_data[1:0] == 2'd3) begin
-                                        // word-aligned 32-bit instruction
-                                        pc <= if_pcnew;
-                                        in_branch <= 0;
-                                        comp_buffer <= 16'd0;
-                                        jump_buffer <= 16'd0;
-                                        filled <= 1'b0;
-                                    end
-                                    else begin
-                                        // word-aligned 16-bit instruction 
-                                        pc <= if_pcnew;
-                                        in_branch <= 0;
-                                        comp_buffer <= inst_data[31:16];
-                                        jump_buffer <= 16'd0;
-                                        filled <= 1'b1; 
-                                    end
-                                end
-                            end
-                        endcase
-                    end
-                    // Reset
-                    default: begin
-                        comp_buffer <= 16'd0;
-                        jump_buffer <= 16'd0;
-                        pc <= 32'd0;
-                        state <= 3'd0;
-                        filled <= 1'b0;
-                        in_branch <= 0;
-                    end
-                endcase
-            end
-        end
-	end
-	
-	// data outputs
+endmodule
+
+module _issue_i(
+    input clk,
+    input nrst,
+
+    input gnt,
+    output req,
+    output ready,
+    
+    input [`WORD_WIDTH-1:0] addr,
+    
+    
+    output reg [`WORD_WIDTH-1:0] addr_out 
+    );
+
+
+
+endmodule
+
+module _buffer_o(
+    input clk,
+    input nrst,
+
+    input hold_in,
+
+    input valid,
+    input align,
+    input flush,
+    input [`WORD_WIDTH-1:0] inst_in
+
+    output compressed_o,
+    output req_d,
+    output ready_o,
+    output reg [`WORD_WIDTH-1:0] inst_out
+    );
+
+    reg [`WORD_WIDTH-1:0] inst_buf;
+    reg [`WORD_WIDTH-1:0] inst_t;
+    reg [15:0] comp_buf;
+    reg fill;
+    reg hold_r;
+    assign compressed_o = align ? comp_buf[1:0] : inst_buf[1:0];
+
+    assign req_d = !fill && align && !compressed_o;
+
+    // Outputs
     always@(*) begin
-        if (branch) begin
-            addr_next = if_pcnew;
-            inst_out_reg = inst_data;
-            // entering control flow instruction
-            if (align_branch && !misalign_in_compressed) begin
-                // misaligned word fetch needed
-                ready_reg = 0;
-            end
-            else begin
-                // no misaligned word fetch
-                ready_reg = 1;
-            end      
+        if (valid) begin
+            inst_out = inst_t;
+            ready_o = !req_d;
         end
-        else if (in_branch) begin
-            addr_next = pc + 32'd4;
-            ready_reg = 1;
-            inst_out_reg = {inst_hi16, inst_lo16};
+        else if (hold_r) begin
+            inst_out = inst_buf;
+            ready_o = 1;
         end
-        
         else begin
-            // regular instruction
-            if (!align || inst_compressed) begin
-                // word-aligned instruction
-                addr_next = pc;
-                ready_reg = 1;
+            inst_out = 0;
+            ready_o = 0;
+        end
+    end
+
+    // Temp instruction
+    always@(*) begin
+        if (valid) begin
+            if (align)
+                inst_t = {comp_buf, inst_in[31:16]};
+            else   
+                inst_t = inst_in;
+        end
+        else begin
+            inst_t = 0;
+        end
+    end
+    
+    // Control + buffer contents
+    always@(posedge clk) begin
+        if (!nrst) begin
+            inst_buf <= 32'd0;
+            comp_buf <= 16'd0;
+            fill <= 0;
+        end
+        else begin
+            // update for new data coming in
+            if (valid) begin
+                inst_buf <= inst_t;
+                comp_buf <= inst_in[31:16];
+                fill <= (align && !compressed_o);
             end
+            // Remove contents for branches
+            else if (flush) begin
+                inst_buf <= 32'd0;
+                comp_buf <= 16'd0;
+                fill <= 0;
+            end
+            // hold for now
             else begin
-                // non-word-aligned instruction
-                if (filled) begin
-                    addr_next = pc + 32'd4;
-                    ready_reg = 1;
-                end
-                else begin
-                    addr_next = pc;
-                    ready_reg = 0;
-                end 
+                inst_buf <= inst_buf;
+                comp_buf <= comp_buf;
+                fill <= fill;
             end
-            inst_out_reg = {inst_hi16, inst_lo16};
-         end
-     end
-     */
+        end
+    end
+
+    always@(posedge clk) begin
+        if (!nrst)
+            hold_r <= 0;
+        else
+            hold_r <= hold;
+    end
 endmodule
