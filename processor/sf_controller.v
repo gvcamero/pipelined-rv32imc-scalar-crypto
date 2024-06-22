ad3dc8cf177be47381278069ae313c5c4cf35923
Allen Jason Tan <allen.jason.tan@eee.upd.edu.ph>
2024-06-22 13:12:20 +0800

    Fix post-synthesis looping jumps

--- processor/sf_controller.v
+++ processor/sf_controller.v
@@ -254,7 +254,7 @@ module sf_controller(
     // Flushes/Resets
     assign if_flush = ISR_PC_flush;
     assign id_flush = (ISR_pipe_flush || jump_flush || branch_flush) || !if_ready;
-    assign exe_flush = exe_jalr_hazard || branch_flush;
+    assign exe_flush = exe_jalr_hazard || branch_flush || (is_nop && ~mem_stall);
     assign mem_flush = (div_running || mul_stall) || (load_hazard && dmem_ready);	
     assign wb_flush = mem_stall;
 
