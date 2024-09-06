4ce696eb5c0babd7e62e057c62552a9841384134
Allen Tan <allen.jason.tan@eee.upd.edu.ph>
2024-09-06 13:33:10 +0800

    Remove 0-cycle Datamem Interface Cases

--- processor/mem_protocol_driver.sv
+++ processor/mem_protocol_driver.sv
@@ -126,15 +126,9 @@ module mem_protocol_driver (
                     if (gnt) begin
                         req <= 0;
                         addr_buffer <= 0;
-                        if (valid) begin
-                            // Ideal load case for testbench
-                            load_buffer <= read_in;
-                            mem_state <= MEM_VALID_LOAD;
-                        end
-                        else begin
-                            load_buffer <= 0;
-                            mem_state <= MEM_GRANT_LOAD;
-                        end
+                        // 1+ cycle delay
+                        load_buffer <= 0;
+                        mem_state <= MEM_GRANT_LOAD;
                     end
                     else begin
                         addr_buffer <= addr_buffer;
@@ -205,12 +199,7 @@ module mem_protocol_driver (
         else begin
             case(mem_state)
                 MEM_WAIT_LOAD: begin
-                    if (valid) begin
-                        ready <= 1;
-                    end
-                    else begin
-                        ready <= 0;
-                    end
+                    ready <= 0;
                 end
                 MEM_GRANT_LOAD: begin
                     if (valid) begin
